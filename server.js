98a1c40daebd6a17ee5a2516fa3547ebb68f7286
shaniece crumpler <s.crumpler11@gmail.com>
2019-05-12 08:58:35 -0400

    second commit

--- server.js
+++ server.js
@@ -7,3 +7,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3005
 
+// middleware below
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// route setup below
+
+app.get('/', async (req, response) => {
+    try {
+      response.json({
+        msg: 'Welcome to Restaurant Application'
+      })
+    } catch (e) {
+      response.status(500).json({ msg: e.status })
+    }
+  });
