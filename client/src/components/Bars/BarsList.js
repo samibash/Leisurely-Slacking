e5dffd23309b0b697fa4cb770c0f1b128f7952fe
Fahad Hussain <sfahadh14@gmail.com>
2019-05-13 15:38:34 -0400

    set up Routes and Links

---
+++ client/src/components/Bars/BarsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BarsList() {
+    return (
+        <div>
+            <h1>This is the list of bars page</h1>
+        </div>
+    )
+}
+
+export default BarsList
