473dc8f2ccb786f73ef223cac1a0e99ce8d61af3
Sami Bachir <samibachir20@email.com>
2019-05-13 12:28:00 -0400

    filled out barRouter

---
+++ routes/barRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { Bar } = require('../models');
+const barRouter = express.Router();
+
+  
+  barRouter.get('/bars', async (req, res) => {
+    try {
+        const allbars = await Bar.findAll()
+      res.json(allbars)
+    } catch (error) {
+      res.json({ msg: error.status })
+    }
+  });
