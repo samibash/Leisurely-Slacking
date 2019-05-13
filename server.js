10ebd69f9905311cc87a2dc0e733d1b36b9bebcc
shaniece crumpler <s.crumpler11@gmail.com>
2019-05-13 19:34:56 -0400

    fixed routes in server

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/destinations', destinationRouter);
-app.use('/', barRouter);
-app.use('/', hotelRouter);
-app.use('/', reviewRouter);
+app.use('/bars', barRouter);
+app.use('/hotels', hotelRouter);
+app.use('/reviews', reviewRouter);
 
 //   server below
 
