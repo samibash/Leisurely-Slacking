36a0d5cfbc614103fd5a548b221697d8efccd9a5
Fahad Hussain <sfahadh14@gmail.com>
2019-05-13 16:07:59 -0400

    set up destinations page blueprint

--- client/src/components/Destinations/Destination.js
+++ client/src/components/Destinations/Destination.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Destination.css'
 
 function Destination() {
     return (
         <div>
             <h1>This is the destination page</h1>
+            <div className="destination">
+                <h2>Destination Name</h2>
+                <div className="destination-image">IMAGE_URL</div>
+                <p>DESCRIPTION_OF_DESTINATION</p>
+                <p>Best place to stay: HOTEL_NAME</p>
+                <p>DESCRIPTION_OF_HOTEL</p>
+                <p>Link: HOTEL_LINK</p>
+            </div>
         </div>
     )
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <div>
-            <h1>This is the navbar page</h1>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
