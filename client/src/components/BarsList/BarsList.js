b3ef386f17c1ab40fa082c85f4417ba818aad9a2
shaniece crumpler <s.crumpler11@gmail.com>
2019-05-14 09:09:26 -0400

    ...

--- client/src/components/BarsList/BarsList.js
+++ client/src/components/BarsList/BarsList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import BarLocation from '../BarLocation/BarLocation'
 
 
-const BarsList = ({}) =>{
+const BarsList = ({bars}) =>{
   
-
+    const barData =  bars && bars.map((bars,index)=>{
+        return <BarLocation key={index} bars={bars}/>
+         })
+        
+     return(
+         <div className="DestinationList">
+             <h1>{barData}</h1>
+            
+         </div>
+     )
 
 
 }
