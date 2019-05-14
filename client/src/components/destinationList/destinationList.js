a1fcbb96928b942664bc6154c0b8fde923935364
Fahad Hussain <sfahadh14@gmail.com>
2019-05-14 09:35:38 -0400

    fixed errors

--- client/src/components/destinationList/destinationList.js
+++ client/src/components/destinationList/destinationList.js
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from 'react';
 import CityLocation from '../CityLocation/CityLocation'
 
 
-const DestinationList = ({destinations}) =>{
+const DestinationList = ({destinations}) => {
   
-
-    const data =  destinations && destinations.map((destination,index)=>{
-   return <CityLocation key={index} destination={destination}/>
+    const data = destinations && destinations.map((destination,index) => {
+        return <CityLocation key={index} destination={destination}/>
     })
    
-return(
-    <div className="DestinationList">
-        <h1>{data}</h1>
-       
-    </div>
-)
-
+    return(
+        <div className="DestinationList">
+            <h1>{data}</h1>
+        </div>
+    )
 }
 
 export default DestinationList
