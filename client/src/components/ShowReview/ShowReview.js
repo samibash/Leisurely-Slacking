db727ba831c108bab4721ccd931a808ec6356b53
Sami Bachir <samibachir20@email.com>
2019-05-16 14:15:37 -0400

    fixed navbar and styled reviews page

--- client/src/components/ShowReview/ShowReview.js
+++ client/src/components/ShowReview/ShowReview.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import './ShowReview.css'
 
 const ShowReview = ({ reviews }) => {
 
     return (
-      <div>
-        <h2>{reviews.user_name}</h2>
-        <li>{reviews.caption}</li>
-        <li>{reviews.experience}</li>
-        <li>{reviews.trip_type}</li>
+    <div className="container">
+      <div className="section" id="this-thang">
+        <div className="row columns">
+          <div className="column">
+            <div className="card large">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <h2 className="title is-4 no-padding">{reviews.user_name}</h2>
+                    <p><span className="subtitle is-6"><strong>Caption: </strong>{reviews.caption}</span></p>
+                    <p className="subtitle is-6"><strong>Experience: </strong>{reviews.experience}</p>
+                  </div>
+                </div>
+                <div className="content">
+                <strong>Type: </strong>{reviews.trip_type}
+                  <div className="background-icon"><span className="icon-twitter" /></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
     );
 }
 
