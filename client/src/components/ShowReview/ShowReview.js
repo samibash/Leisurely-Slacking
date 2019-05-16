59f6bd6ee8779f877c9617ebf61192adbe8b9b1e
Sami Bachir <samibachir20@email.com>
2019-05-16 14:50:12 -0400

    styled the bar page

--- client/src/components/ShowReview/ShowReview.js
+++ client/src/components/ShowReview/ShowReview.js
@@ -12,30 +12,30 @@ const ShowReview = ({ reviews }) => {
   }
 
     return (
-    <div className="container">
-      <div className="section" id="this-thang">
-        <div className="row columns">
-          <div className="column">
-            <div className="card large">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-content">
-                    <h2 className="title is-4 no-padding">{reviews.user_name}</h2>
-                    <p><span className="subtitle is-6"><strong>Caption: </strong>{reviews.caption}</span></p>
-                    <p className="subtitle is-6"><strong>Experience: </strong>{reviews.experience}</p>
+      <div className="container">
+        <div className="section" id="this-thang">
+          <div className="row columns">
+            <div className="column">
+              <div className="card large">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-content">
+                      <h2 className="title is-4 no-padding">{reviews.user_name}</h2>
+                      <p><span className="subtitle is-6"><strong>Caption: </strong>{reviews.caption}</span></p>
+                      <p className="subtitle is-6"><strong>Experience: </strong>{reviews.experience}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="content">
-                <strong>Type: </strong>{reviews.trip_type}
-                  <div className="background-icon"><span className="icon-twitter" /></div>
-              <button onClick={() => onReviewDelete(reviews)}>DELETE</button>
+                  <div className="content">
+                  <strong>Type: </strong>{reviews.trip_type}
+                    <div className="background-icon"><span className="icon-twitter" /></div>
+                <button onClick={() => onReviewDelete(reviews)}>DELETE</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     );
 }
 
